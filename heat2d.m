602140d579f86268aa83f3d54b40cf7bc5ee0d5a
DanmerZ <oleh-mail@yandex.ru>
2015-07-22 18:36:48 +0300

    working wave Euler

--- heat2d.m
+++ heat2d.m
@@ -9,10 +9,10 @@ dx = 2*pi/NX; dy = 2*pi/NY;
 [i,j] = meshgrid(1:NX,1:NY);
 x = i*dx; y = j*dy;
 
-t0 = 0.0; tSteps = 150000;
+t0 = 0.0; tSteps = 30000;
 dt = 0.001*dx;
 
-shfactor = 300;
+shfactor = 1000;
 nshow = tSteps / shfactor;
 
 ikx = 1i*ones(1,NY)'*[0:NX/2 -NX/2+1:-1];
@@ -24,9 +24,9 @@ iky2 = iky.*iky;
 u = zeros(nshow,NX,NY);
 uu = zeros(nshow,NX,NY);
 
-uold = exp(-((x-pi).^2 + (y-pi).^2));
+uold = .1*exp(-100*((x-pi).^2 + (y-pi).^2));
 unew = uold;
-uuold = -exp(-((x-pi).^2 + (y-pi).^2)); % uold;
+uuold = .1*exp(-100*((x-pi).^2 + (y-pi).^2)); % uold;
 uunew = uuold;
 
 u(1,:,:) = reshape(uold,1,NX,NY);
@@ -34,7 +34,7 @@ uu(1,:,:) = reshape(uuold,1,NX,NY);
 
 for tstep = 1:tSteps-1
     uold = unew; uuold = uunew;
-    uf = fftn(uuold); % fftn(squeeze(u(tstep,:,:)));
+    uf = fftn(uold); % fftn(squeeze(u(tstep,:,:)));
 %     uxx = real(ifft2(ikx2.*uf));
 %     uyy = real(ifft2(iky2.*uf));
 %     [uxx,uyy] = sd2_2d(uf,ikx2,iky2);
@@ -53,13 +53,20 @@ for tstep = 1:tSteps-1
      unew(:,end) = 0.0; unew(end,:) = 0.0;
 end
 
-
-
+giffile = 'movie3.gif';
 
 for tstep=1:nshow-1
 %     , axis([0 NX 0 NY 0 1]),
-    surf(squeeze(u(tstep,:,:))),title(sprintf('t=%d',tstep)),axis([0 NX 0 NY 0 1]),  drawnow;    
-    M(tstep) = getframe;    
+    mesh(squeeze(u(tstep,:,:))),title(sprintf('t=%d',tstep)),axis([0 NX 0 NY -.05 .05]),  drawnow;    
+%     M(tstep) = getframe;   
+    frame = getframe;
+    im = frame2im(frame);
+    [imind,cm] = rgb2ind(im,256);
+    if tstep == 1;
+        imwrite(imind,cm,giffile,'gif','Loopcount',inf);
+    else
+        imwrite(imind,cm,giffile,'gif','WriteMode','append','DelayTime',.05);
+    end
 end
 
 
