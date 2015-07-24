cb8d168b583f7f4272442ff8367a76a36770195f
DanmerZ <oleh-mail@yandex.ru>
2015-07-24 17:56:36 +0300

    convolution 1d

---
+++ convolution.m
@@ -0,0 +1,26 @@
+clear all;
+clc;
+close all;
+
+NX = 512;
+dx = 2*pi/NX;
+
+i = meshgrid(1:NX,1);
+x = i*dx;
+
+kx = [0:NX/2 -NX/2+1:-1];
+
+f = exp(-(x-pi).^2);
+ff = f.*f;
+dffx = -4*(x-pi).*ff;
+
+ffhat = fft(ff);
+
+ffhat2 = ifft( fft(fft(f)).^2 );
+
+fhat = fft(f);
+cnv = conv(fhat,fhat,'same');
+
+
+% plot(x,-dffx+real(ifft(1i*kx.*ffhat2))/NX)
+plot(x,-dffx+real(ifft(1i*kx.*cnv))/NX)
